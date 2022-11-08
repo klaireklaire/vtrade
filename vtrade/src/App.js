35bcf9949347efe57fd8d1d00bcfdbd23a1b27e9
klaireklaire <khlinph@gmail.com>
2022-11-08 10:53:37 -0500

    make compoenents folder

--- vtrade/src/App.js
+++ vtrade/src/App.js
@@ -1,18 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Categories from "./Categories";
+import Categories from "./Components/Categories";
 
 function App() {
   return (
     <div class>
-      <div>Navbar</div>
+      <div>Navbar Placeholder</div>
       <div>
         <Categories />
       </div>
-      <div>Highlights</div>
+      <div>Highlights Placeholder</div>
       <div>
-        <div>Recent Posts</div>
-        <div>Recent Events</div>
+        <div>Recent Posts Placeholder</div>
+        <div>Recent Events Placeholder</div>
       </div>
     </div>
   );
