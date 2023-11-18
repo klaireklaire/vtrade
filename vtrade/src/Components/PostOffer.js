6457337e79f160e4801dfd2f64ac0165465005b5
Jae Young Seo <jaeyoungseo@jys.local>
2023-11-17 20:53:51 -0500

    Integrating BackEnd

--- vtrade/src/Components/PostOffer.js
+++ vtrade/src/Components/PostOffer.js
@@ -21,10 +21,11 @@ export default function PostOffer(props) {
   const [location, setLocation] = React.useState("");
   const [free, setFree] = React.useState(false);
   const [customLocation, setCustomLocation] = useState("");
-  const [price, setPrice] = React.useState(null);
+  const [price, setPrice] = React.useState(0.0);
   const [method, setMethod] = React.useState(null);
   const [page, setPage] = React.useState(1);
   const [images, setImages] = React.useState([]);
+  const [stringPrice, setStringPrice] = React.useState("");
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
   const navigate = useNavigate();
@@ -35,30 +36,35 @@ export default function PostOffer(props) {
   const forFree = "For Free";
 
   const handleOnSubmit = async () => {
+    console.log(images);
     const pictures = images.map((image) => image.file);
+    console.log(pictures);
 
-    //update this
-    const { data, error } = await apiClient.postItem({
-      userId: props.user.id,
-      title: title,
-      category: category,
-      condition: condition,
-      price: price,
-      description: description,
-      location: location,
-      method: method,
-      pictures,
-    });
-    console.log(data);
-
-    if (data) {
-      navigate("/");
-      props.setIsLoading(false);
-    } else {
-      console.log(error);
+    try {
+      const { data, error } = await apiClient.postListing({
+        user_id: props.user.id,
+        listingtype: 0,
+        title: title,
+        form: 1,
+        status: "avaliable",
+        category: category,
+        condition: condition,
+        price: price,
+        description: description,
+        location: location,
+        payment: method,
+        type: "sell",
+        images,
+      });
+      console.log(data);
+      if (data) {
+        navigate("/");
+      } else {
+        console.log(error);
+      }
+    } catch {
+      console.log("error");
     }
-
-    props.setIsLoading(false);
   };
 
   const handleOnInputChange = (event) => {
@@ -90,15 +96,44 @@ export default function PostOffer(props) {
 
   const handleOnPriceInputChange = (value) => {
     if (value) {
-      setPrice(0);
+      setStringPrice("$ Price your listing");
+      setPrice(0.0);
       setFree(true);
     } else {
       setFree(false);
-      setPrice(null);
+      setPrice(0.0);
     }
   };
 
-  const updateParentRemovedImage = (index) => {
+  const handleOnPriceChange = (event) => {
+    const userInput = event.target.value;
+    console.log("User Input:", userInput);
+
+    const cleanedInput = userInput.replace(/[^0-9.]/g, "");
+    console.log("Cleaned Input:", cleanedInput);
+
+    const sanitizedInput = cleanedInput
+      .split(".")
+      .map((part, index) => (index === 1 ? part.slice(0, 2) : part))
+      .join(".");
+    console.log("Sanitized Input:", sanitizedInput);
+
+    if (sanitizedInput === "" || sanitizedInput === ".") {
+      console.log("Setting Price and StringPrice to null and empty string");
+      setPrice(null);
+      setStringPrice("");
+    } else {
+      console.log(
+        "Setting Price and StringPrice:",
+        parseFloat(sanitizedInput),
+        "$" + sanitizedInput
+      );
+      setPrice(parseFloat(sanitizedInput));
+      setStringPrice("$" + sanitizedInput);
+    }
+  };
+
+  const updateRemovedImage = (index) => {
     const newImages = [...images];
     newImages.splice(index, 1);
     setImages(newImages);
@@ -109,41 +144,82 @@ export default function PostOffer(props) {
     }
   };
 
-  const updateParentFile = (event) => {
-    const files = Array.from(event.target.files);
-    setImages([...images, ...files]);
-    setSelectedImageIndex(images.length); // Select the last uploaded image
+  const handleFileInputChange = (event) => {
+    if (images.length < 7) {
+      const files = Array.from(event.target.files);
+      setImages([...images, ...files]);
+      setSelectedImageIndex(images.length); // Select the last uploaded image
+    } else {
+      alert("You can only upload a maximum of 7 photos.");
+    }
   };
 
-  const updateParentSelectedImageIndex = (index) => {
+  const updateSelectedImageIndex = (index) => {
     setSelectedImageIndex(index);
   };
-
-  const imageUploadComponent = useMemo(
-    () => (
-      <ImageUpload
-        updateParentFile={updateParentFile}
-        images={images}
-        selectedImageIndex={selectedImageIndex}
-      />
-    ),
-    [updateParentFile, images, selectedImageIndex]
-  );
 
   return (
     <div>
       {props.isLoading ? (
         props.loader()
       ) : (
-        <div>
+        <div className="mb-10">
           <div className="flex flex-row mt-10">
             <div className="flex flex-col">
-              {imageUploadComponent}
-              <ImageScroll
-                updateParentRemovedImage={updateParentRemovedImage}
-                updateParentSelectedImageIndex={updateParentSelectedImageIndex}
-                images={images}
-              />
+              <div
+                className="ml-[77px] mr-[55px] h-[440px] w-[640px] mx-auto rounded-2xl bg-white border-dashed border-2 border-gray-300 overflow-hidden flex flex-col items-center justify-center"
+                onDragOver={handleFileInputChange}
+                onDragLeave={handleFileInputChange}
+                onDrop={handleFileInputChange}
+                onClick={() => document.getElementById("file-input").click()}
+              >
+                <input
+                  type="file"
+                  id="file-input"
+                  hidden
+                  onChange={handleFileInputChange}
+                  accept="image/*"
+                  multiple
+                />
+                {images.length === 0 ? (
+                  <p className="text-light-black font-mulish text-lg text-center font-semibold tracking-wide">
+                    Click or Drag Photos Here
+                  </p>
+                ) : (
+                  <img
+                    class="lazy"
+                    src={URL.createObjectURL(images[selectedImageIndex])}
+                    alt={`Image ${selectedImageIndex}`}
+                    className="max-h-full max-w-full mx-auto"
+                  />
+                )}
+              </div>
+
+              {images.length > 0 && (
+                <div
+                  className="overflow-x-auto whitespace-nowrap ml-[77px]"
+                  style={{ maxWidth: "640px" }}
+                >
+                  <div className="flex flex-nowrap justify-start mt-3 ">
+                    {images.map((file, index) => (
+                      <div key={index} className="relative m-2">
+                        <button
+                          className="flex items-center justify-center absolute -top-2 -right-2 w-6 h-6 p-1 bg-black rounded-full text-white text-xs cursor-pointer hover:bg-gray-700"
+                          onClick={() => updateRemovedImage(index)}
+                        >
+                          x
+                        </button>
+                        <img
+                          src={URL.createObjectURL(file)}
+                          alt={`Image ${index + 1}`}
+                          onClick={() => updateSelectedImageIndex(index)}
+                          className="max-h-[100px] max-w-[100px] cursor-pointer"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
             </div>
             <div className="flex flex-col justify-top items-start flex-shrink-0">
               <Title
@@ -194,15 +270,15 @@ export default function PostOffer(props) {
                 </label>
               </div>
               <input
-                type="number"
+                type="text"
                 className={`border border-solid border-black bg-white h-11 rounded-md p-4 w-full mt-4 text-gray-500 text-base font-mulish font-normal leading-5 tracking-wider ${
                   free ? "opacity-25" : ""
                 }`}
                 placeholder="$ Price your listing"
                 disabled={free}
-                value={price}
+                value={stringPrice}
                 onChange={(event) => {
-                  setPrice(event.target.value);
+                  handleOnPriceChange(event);
                 }}
               />
               <p className="mt-4 text-light-black font-mulish text-basePlus font-normal tracking-wide">
