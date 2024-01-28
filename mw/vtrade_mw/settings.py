7e448f688ea792c73a880f8f60b42bd8fe4b7581
Victor Zhang <victormathwiz@gmail.com>
2024-01-28 14:42:13 -0500

    connecting django to postsql database

--- mw/vtrade_mw/settings.py
+++ mw/vtrade_mw/settings.py
@@ -77,9 +77,11 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        'USER': "",
+        "PASSWORD": "PASSWORD",
+        "HOST": "localhostport:5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
