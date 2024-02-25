ff86073c35b3e00c588dba4ac6898d4fd8849e76
Jae Young Seo <jaeyoungseo@jys.local>
2024-02-25 14:54:55 -0500

    add_user route

--- mw/vtrade/views.py
+++ mw/vtrade/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
+from . import routes
 
 
 def get_user(request):
     user_input = request.GET.get("input")
-
-    return user
+    response = routes.add_user(user_input)
 
 
 def post_listing(request):
